baf3f7aecf7e8c2a2408d2a280d36ad7f4a55cf6
pwebb27 <p-webb27@hotmail.com>
2022-04-08 10:25:50 -0500

    other: Added wakelock for keeping screen on when coding

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:curl_manitoba/route_generator.dart';
 import 'package:curl_manitoba/screens/news_story_screen.dart';
 import 'package:curl_manitoba/screens/tabs_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:wakelock/wakelock.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Wakelock.enable();
+    
     return MaterialApp(
         theme: ThemeData(
             primaryColor: Color.fromRGBO(111, 18, 0, 1),
