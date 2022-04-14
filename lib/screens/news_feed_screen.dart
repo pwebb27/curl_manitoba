77e22592552a3cc5dc0cd6e516ef4569bd6fd6a0
pwebb27 <p-webb27@hotmail.com>
2022-04-14 09:58:25 -0500

    Fix: Fix layout of widgets in NewsFeedScreen

--- lib/screens/news_feed_screen.dart
+++ lib/screens/news_feed_screen.dart
@@ -63,33 +63,82 @@ class _NewsFeedScreenState extends State<NewsFeedScreen> {
           } else
             buildContent(snapshot.data as dom.Document);
           return Scaffold(
-            body: ListView.builder(
-                itemBuilder: (ctx, index) {
-                  NewsStory newsStory = NewsStory(
-                    id: index,
-                    imageURL:
-                        'https://images.thestar.com/CBZVV_aqoiPFukcZjs74JNLtlF8=/1200x798/smart/filters:cb(2700061000)/https://www.thestar.com/content/dam/thestar/sports/curling/2018/02/04/manitobas-jennifer-jones-heads-to-scotties-tournament-of-hearts-final/jennifer_jones.jpg',
-                    headline: titles[index],
-                    author: authors[index],
-                    date: dates[index],
-                    content: content[index],
-                  );
-                  return buildNewsStoryItem(
-                    newsStory,
-                  );
-                },
-                itemCount: 8),
-          );
+              body: SingleChildScrollView(
+                physics: ScrollPhysics(),
+                child: Column(children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8,left:8,right:8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [Text('Sign up for our ', style: TextStyle(fontSize: 16)), GestureDetector(child: Text('newsletter', style: TextStyle(fontSize: 15, color: Colors.blue)),
+                            )]),
+                            ),
+                          ListView.builder(
+                            physics: NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                  itemBuilder: (ctx, index) {
+                    NewsStory newsStory = NewsStory(
+                      id: index,
+                      imageURL:
+                          'https://images.thestar.com/CBZVV_aqoiPFukcZjs74JNLtlF8=/1200x798/smart/filters:cb(2700061000)/https://www.thestar.com/content/dam/thestar/sports/curling/2018/02/04/manitobas-jennifer-jones-heads-to-scotties-tournament-of-hearts-final/jennifer_jones.jpg',
+                      headline: titles[index],
+                      author: authors[index],
+                      date: dates[index],
+                      content: content[index],
+                    );
+                    return buildNewsStoryItem(
+                      newsStory,
+                    );
+                  },
+                  itemCount: 8),
+                        ]),
+              ));
         });
   }
 }
 
 buildNewsStoryItem(NewsStory newsStory) {
   return Card(
-      child: Row(children: [
-    Image.network(newsStory.imageURL, height: 30),
-    Column(
-      children: [Text(newsStory.date), Text(newsStory.headline)],
-    )
-  ]));
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: Colors.grey.shade700, width: .3),
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      clipBehavior: Clip.antiAlias,
+      child: IntrinsicHeight(
+        child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Expanded(
+            flex: 20,
+            child: Image.network(
+              newsStory.imageURL,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            flex: 30,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 7.0),
+                    child: Text(
+                      newsStory.date,
+                      style:
+                          TextStyle(fontSize: 13, color: Colors.grey.shade700),
+                    ),
+                  ),
+                  Text(newsStory.headline + '\n\n\n',
+                      maxLines: 4,
+                      overflow: TextOverflow.ellipsis,
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold))
+                ],
+              ),
+            ),
+          ),
+        ]),
+      ));
 }
