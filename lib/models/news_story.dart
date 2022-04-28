0f0b6215f479b38c0c99eb72c5aca703fa0b2c21
pwebb27 <p-webb27@hotmail.com>
2022-04-28 10:22:46 -0500

    refactor: Refactor newsStory model to include json constructor

--- lib/models/news_story.dart
+++ lib/models/news_story.dart
@@ -1,13 +1,27 @@
 class NewsStory {
+  late String headline;
+  late String imageURL;
+  late String author;
+  late String date;
+  late int id;
+  String? content;
 
-  final String headline;
-  final String imageURL;
-  final String author;
-  final String date;
-  final int id;
-  final String content;
+  NewsStory.fromJson(Map<String, dynamic> json) {
+    this.id = json['id'];
+    this.author = getAuthorId(json['author']);
+    this.headline = json['title']['rendered'];
+    this.date = json['date'];
+    this.imageURL = 'https://images.thestar.com/CBZVV_aqoiPFukcZjs74JNLtlF8=/1200x798/smart/filters:cb(2700061000)/https://www.thestar.com/content/dam/thestar/sports/curling/2018/02/04/manitobas-jennifer-jones-heads-to-scotties-tournament-of-hearts-final/jennifer_jones.jpg';
+  }
 
-
-  NewsStory({required this.id, required this.headline,required this.imageURL, required this.date, required this.author, required this.content});
-
+  String getAuthorId(int authorId) {
+    switch (authorId) {
+      case 3:
+        return 'Laurie Macdonell';
+      case 27:
+        return 'Rob Gordon';
+      default:
+        return 'Unknown author';
+    }
+  }
 }
