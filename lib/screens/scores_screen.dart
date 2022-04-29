d2d7a9105e378e972ec163987bfe6431feba7ffa
pwebb27 <p-webb27@hotmail.com>
2022-04-29 09:20:44 -0500

    fix: Add navigation to scoresWebPageScreen for competition tiles on NewsFeedScreen

--- lib/screens/scores_screen.dart
+++ lib/screens/scores_screen.dart
@@ -46,62 +46,65 @@ Future<List<dynamic>> _getDataFromWeb() async {
   return responses;
 }
 
-buildCompetitionTile(Map competition) {
-  return Card(
-    shape: RoundedRectangleBorder(
-      side: BorderSide(color: Colors.grey.shade700, width: .3),
-      borderRadius: BorderRadius.circular(10),
-    ),
-    elevation: 1.5,
-    child: Padding(
-      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Text(competition["title"],
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
-        Padding(
-          padding: const EdgeInsets.only(top: 7),
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Row(children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 1.5),
-                  child: Icon(FontAwesomePro.calendar_range,
-                      size: 10, color: Colors.grey.shade700),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 5),
-                  child: Text(
-                    DateFormat('LLL d, y').format(
-                          DateTime.parse(competition['starts_on']),
-                        ) +
-                        ' - ' +
-                        DateFormat('LLL d, y')
-                            .format(DateTime.parse(competition['ends_on'])),
-                    style: TextStyle(fontSize: 12, color: Colors.grey.shade700),
+buildCompetitionTile(BuildContext context,Map competition) {
+  return InkWell(
+    onTap:(){ Navigator.pushNamed(context, '/scoresWebPage', arguments: competition["id"].toString());},
+    child: Card(
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: Colors.grey.shade700, width: .3),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 1.5,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(competition["title"] + '\n', maxLines: 2,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+          Padding(
+            padding: const EdgeInsets.only(top: 7),
+            child:
+                Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Row(children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 1.5),
+                    child: Icon(FontAwesomePro.calendar_range,
+                        size: 10, color: Colors.grey.shade700),
                   ),
-                ),
-              ]),
-              Row(children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 1.5),
-                  child: Icon(FontAwesomePro.location_dot,
-                      size: 10, color: Colors.grey.shade700),
-                ),
-                Padding(
+                  Padding(
                     padding: const EdgeInsets.only(left: 5),
-                    child: Text(getVenue(competition),
-                        style: TextStyle(
-                            fontSize: 12, color: Colors.grey.shade700)))
-              ])
+                    child: Text(
+                      DateFormat('LLL d, y').format(
+                            DateTime.parse(competition['starts_on']),
+                          ) +
+                          ' - ' +
+                          DateFormat('LLL d, y')
+                              .format(DateTime.parse(competition['ends_on'])),
+                      style: TextStyle(fontSize: 12, color: Colors.grey.shade700),
+                    ),
+                  ),
+                ]),
+                Row(children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 1.5),
+                    child: Icon(FontAwesomePro.location_dot,
+                        size: 10, color: Colors.grey.shade700),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: Text(getVenue(competition),
+                          style: TextStyle(
+                              fontSize: 12, color: Colors.grey.shade700)))
+                ])
+              ]),
+              Image.network(
+                competition["logo"],
+                height: 30,
+              ),
             ]),
-            Image.network(
-              competition["logo"],
-              height: 30,
-            ),
-          ]),
-        ),
-      ]),
+          ),
+        ]),
+      ),
     ),
   );
 }
@@ -193,7 +196,7 @@ class _ScoresScreenState extends State<ScoresScreen> {
                       physics: NeverScrollableScrollPhysics(),
                       shrinkWrap: true,
                       itemBuilder: (ctx, index) {
-                        return buildCompetitionTile(competitions[index]);
+                        return buildCompetitionTile(context,competitions[index]);
                       },
                       itemCount: 8),
                 ),
