089345906d3e0a5abbc4e7fc610e1deb048e4b3c
pwebb27 <p-webb27@hotmail.com>
2022-04-29 09:38:12 -0500

    fix: Add fixes for API, and navigation and add end of scroll prompt
    - Use WordPress API instead of HTML docuement
    - Add navigation for news story items to NewsStoryScreen
    - Add prompt at end of scroll for visiting news archive

--- lib/screens/news_feed_screen.dart
+++ lib/screens/news_feed_screen.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:curl_manitoba/widgets/custom_app_bar.dart';
 import 'package:curl_manitoba/widgets/font_awesome_pro_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/dom.dart' as dom;
 import 'package:html/parser.dart' as parser;
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../models/news_story.dart';
 import '../widgets/circular_progress_bar.dart';
@@ -16,36 +19,21 @@ class NewsFeedScreen extends StatefulWidget {
 class _NewsFeedScreenState extends State<NewsFeedScreen> {
   static const routeName = '/news';
 
-  List<String> titles = [];
-  List<String> dates = [];
-  List<String> authors = [];
-  List<String> content = [];
+  List<NewsStory> newsStories = [];
 
-  static const URL = 'https://curlmanitoba.org/news-2/news-archive/';
+  static const URL = 'https://curlmanitoba.org/wp-json/wp/v2/posts?_fields=id,title,date,author';
 
-  Future<dom.Document> _getDataFromWeb() async {
-    final response = await http.get(Uri.parse(URL));
+Future<List<dynamic>> _getDataFromWeb() async {
+ var response = await 
+    http.get(Uri.parse(URL));
+  
+  return json.decode(response.body);
+}
 
-    final body = response.body;
-    dom.Document document = parser.parse(body);
-
-    return document;
-  }
-
-  void buildContent(dom.Document document) {
-    final titleElements = document.getElementsByClassName('entry-title');
-    titles = titleElements
-        .map((element) => element.getElementsByTagName("a")[0].innerHtml)
-        .toList();
-
-    final dateElements = document.getElementsByTagName('time');
-    dates = dateElements.map((element) => element.innerHtml).toList();
-
-    final authorElements = document.getElementsByClassName('fn');
-    authors = authorElements.map((element) => element.innerHtml).toList();
-
-    final contentElements = document.getElementsByTagName("p");
-    content = contentElements.map((element) => element.innerHtml).toList();
+  void buildContent(List<dynamic> posts) {
+    posts.forEach((post) { 
+      newsStories.add(NewsStory.fromJson(post));
+    });
   }
 
   @override
@@ -61,7 +49,7 @@ class _NewsFeedScreenState extends State<NewsFeedScreen> {
           if (snapshot.data == null) {
             return CircularProgressBar();
           } else
-            buildContent(snapshot.data as dom.Document);
+            buildContent(snapshot.data as List<dynamic>);
           return Scaffold(
               body: SingleChildScrollView(
                 physics: ScrollPhysics(),
@@ -77,68 +65,71 @@ class _NewsFeedScreenState extends State<NewsFeedScreen> {
                             physics: NeverScrollableScrollPhysics(),
                             shrinkWrap: true,
                   itemBuilder: (ctx, index) {
-                    NewsStory newsStory = NewsStory(
-                      id: index,
-                      imageURL:
-                          'https://images.thestar.com/CBZVV_aqoiPFukcZjs74JNLtlF8=/1200x798/smart/filters:cb(2700061000)/https://www.thestar.com/content/dam/thestar/sports/curling/2018/02/04/manitobas-jennifer-jones-heads-to-scotties-tournament-of-hearts-final/jennifer_jones.jpg',
-                      headline: titles[index],
-                      author: authors[index],
-                      date: dates[index],
-                      content: content[index],
-                    );
-                    return buildNewsStoryItem(
-                      newsStory,
+                    return buildNewsStoryItem(context,
+                      newsStories[index],
                     );
                   },
-                  itemCount: 8),
+                  itemCount: newsStories.length),
+                  Divider(),
+                  SvgPicture.asset('assets/icons/folder.svg'),
+                  Text('Looking for more news?', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold )),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 13, top: 5, left: 8, right: 8.0),
+                    child: Text('Visit the new archive for a history of published articles',textAlign: TextAlign.center, style: TextStyle(fontSize: 15),),
+                  )
                         ]),
               ));
         });
   }
 }
 
-buildNewsStoryItem(NewsStory newsStory) {
-  return Card(
-      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      shape: RoundedRectangleBorder(
-        side: BorderSide(color: Colors.grey.shade700, width: .3),
-        borderRadius: BorderRadius.circular(5.0),
-      ),
-      clipBehavior: Clip.antiAlias,
-      child: IntrinsicHeight(
-        child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-          Expanded(
-            flex: 20,
-            child: Image.network(
-              newsStory.imageURL,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Expanded(
-            flex: 30,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 7.0),
-                    child: Text(
-                      newsStory.date,
-                      style:
-                          TextStyle(fontSize: 13, color: Colors.grey.shade700),
-                    ),
-                  ),
-                  Text(newsStory.headline + '\n\n\n',
-                      maxLines: 4,
-                      overflow: TextOverflow.ellipsis,
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold))
-                ],
+buildNewsStoryItem(BuildContext context, NewsStory newsStory) {
+  return InkWell(
+    onTap: () {
+      Navigator.pushNamed(context, '/newsStory', arguments: newsStory);
+    },
+    child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        shape: RoundedRectangleBorder(
+          side: BorderSide(color: Colors.grey.shade700, width: .3),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: IntrinsicHeight(
+          child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            Expanded(
+              flex: 20,
+              child: Image.network(
+                newsStory.imageURL as String,
+                fit: BoxFit.cover,
               ),
             ),
-          ),
-        ]),
-      ));
+            Expanded(
+              flex: 30,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 7.0),
+                      child: Text(
+                        newsStory.date,
+                        style:
+                            TextStyle(fontSize: 13, color: Colors.grey.shade700),
+                      ),
+                    ),
+                    Text(newsStory.headline + '\n\n\n',
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                        style:
+                            TextStyle(fontSize: 15, fontWeight: FontWeight.bold))
+                  ],
+                ),
+              ),
+            ),
+          ]),
+        )),
+  );
 }
