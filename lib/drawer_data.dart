6a9fc43cfaefc91f63a8c75f44e6be123c08342c
pwebb27 <p-webb27@hotmail.com>
2022-05-12 16:19:52 -0500

    style: Adjust icon sizes for drawer data

--- lib/drawer_data.dart
+++ lib/drawer_data.dart
@@ -3,8 +3,8 @@ import 'gridview_tile_data.dart';
 
 
 List<DrawerTile> BASIC_TILES_DATA = [
-  DrawerTile.asPage('Competitions', 'medals', COMPETITION_GRID_VIEW_TILES_DATA, height: 28 ),
-  DrawerTile.asPage('Development', 'gear-arrow',DEVELOPMENT_GRID_VIEW_TILES_DATA,  width: 22.5, height: 22.5),
+  DrawerTile.asPage('Competitions', 'medals', COMPETITION_GRID_VIEW_TILES_DATA, height: 24.5 ),
+  DrawerTile.asPage('Development', 'gear-arrow',DEVELOPMENT_GRID_VIEW_TILES_DATA,  width: 21.5, height: 21.5),
   DrawerTile.asPage('High Perfomance', 'gauge-max',HIGH_PERFORMANCE_GRID_VIEW_TILES_DATA),
   DrawerTile.asPage('Getting Started', 'hourglass-start',GETTING_STARTED_GRID_VIEW_TILES_DATA ),
   DrawerTile.asPage('About Us', 'circle-info',ABOUT_US_TILES_DATA),
@@ -12,9 +12,9 @@ List<DrawerTile> BASIC_TILES_DATA = [
 
 List<DrawerTile> USEFUL_LINKS_DATA = [
   DrawerTile.asUsefulLink('Curling I/O', 'curling-canada',
-      'https://www.curling.ca', height: 28),
+      'https://www.curling.ca', height: 27.5),
   DrawerTile.asUsefulLink('CurlManitoba Twitter', 'twitter',
-      'https://twitter.com/curlmanitoba',width: 22, height: 22),
+      'https://twitter.com/curlmanitoba',width: 21.5, height: 21.5),
   DrawerTile.asUsefulLink('CurlManitoba Instagram', 'instagram',
       'https://www.instagram.com/curlmanitoba/?hl=en', width: 22, height: 22),
   DrawerTile.asUsefulLink('CurlManitoba Facebook', 'facebook',
