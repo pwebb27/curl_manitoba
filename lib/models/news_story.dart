6189f1a412c118df2f1911ee6cc7f5faaf2872fc
pwebb27 <p-webb27@hotmail.com>
2022-05-31 12:22:12 -0500

    refactor: Create news article API call methods into NewsStory class
    - Create methods for getting and parsing API data from news articles in the News Story class

--- lib/models/news_story.dart
+++ lib/models/news_story.dart
@@ -1,4 +1,7 @@
+import 'package:http/http.dart';
 import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class NewsStory {
   late String headline;
@@ -12,9 +15,9 @@ class NewsStory {
     this.id = json['id'];
     this.author = getAuthorId(json['author']);
     this.headline = json['title']['rendered'];
-    this.date = DateFormat('LLL d, y')
-                              .format(DateTime.parse(json['date']));
-    this.imageURL = 'https://images.thestar.com/CBZVV_aqoiPFukcZjs74JNLtlF8=/1200x798/smart/filters:cb(2700061000)/https://www.thestar.com/content/dam/thestar/sports/curling/2018/02/04/manitobas-jennifer-jones-heads-to-scotties-tournament-of-hearts-final/jennifer_jones.jpg';
+    this.date = DateFormat('LLL d, y').format(DateTime.parse(json['date']));
+    this.imageURL =
+        'https://images.thestar.com/CBZVV_aqoiPFukcZjs74JNLtlF8=/1200x798/smart/filters:cb(2700061000)/https://www.thestar.com/content/dam/thestar/sports/curling/2018/02/04/manitobas-jennifer-jones-heads-to-scotties-tournament-of-hearts-final/jennifer_jones.jpg';
   }
 
   String getAuthorId(int authorId) {
@@ -26,5 +29,22 @@ class NewsStory {
       default:
         return 'Unknown author';
     }
+  }
+
+  static Future<Response> getNewsData() async {
+    const newsURL =
+        'https://curlmanitoba.org/wp-json/wp/v2/posts?_fields=id,title,date,author&per_page=4';
+    var response = await http.get(Uri.parse(newsURL));
+    return response;
+  }
+
+  static List<NewsStory> parseNewsData(http.Response newsStoriesResponse) {
+    final posts = json.decode(newsStoriesResponse.body);
+    List<NewsStory> newsStories = [];
+
+    posts.forEach((post) {
+      newsStories.add(NewsStory.fromJson(post));
+    });
+    return newsStories;
   }
 }
