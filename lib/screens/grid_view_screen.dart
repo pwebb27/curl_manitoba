83ccf0e477da2ba8f0c6acf08201b50b64f3e6d9
pwebb27 <p-webb27@hotmail.com>
2022-06-09 12:44:39 -0500

    refactor: Refactor GridViewScreen
    - Pass drawer tile title parameter instead of gridViewTile List
    - Integrate switch statement for drawer tile title and GridViewTileData
    - Refactor GridView builder to generate based off mapping the GridViewTile data

--- lib/screens/grid_view_screen.dart
+++ lib/screens/grid_view_screen.dart
@@ -1,105 +1,104 @@
 import 'package:curl_manitoba/gridview_tile_data.dart';
 import 'package:curl_manitoba/widgets/custom_app_bar.dart';
-import 'package:curl_manitoba/widgets/font_awesome_pro_icons.dart';
 import 'package:curl_manitoba/widgets/grid_view_tile.dart';
 import 'package:flutter/material.dart';
 
 class GridViewScreen extends StatefulWidget {
-  GridViewScreen(this.gridViewArguments);
+  GridViewScreen(this.title);
 
-  Map<String, dynamic> gridViewArguments;
+  String title;
 
   @override
-  State<GridViewScreen> createState() =>
-      _GridViewScreenState();
+  State<GridViewScreen> createState() => _GridViewScreenState();
 }
 
 class _GridViewScreenState extends State<GridViewScreen> {
-  List<Widget> gridViewItems = [];
-  late Map<String, dynamic> gridViewArguments;
+  late String title;
+  late List<gridViewTile> gridViewTiles;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    gridViewArguments = widget.gridViewArguments;
+    title = widget.title;
+    gridViewTiles = getGridViewData(title);
   }
 
+  getGridViewData(String title) {
+    switch (title) {
+      case 'Competitions':
+        return COMPETITION_GRID_VIEW_TILES_DATA;
+      case 'Development':
+        return DEVELOPMENT_GRID_VIEW_TILES_DATA;
+      case 'High Performance':
+        return HIGH_PERFORMANCE_GRID_VIEW_TILES_DATA;
+      case 'Getting Started':
+        return GETTING_STARTED_GRID_VIEW_TILES_DATA;
+      case 'About Us':
+        return ABOUT_US_TILES_DATA;
+    }
+  }
 
   @override
   Widget build(
     BuildContext context,
   ) {
-    String Pagetitle = gridViewArguments['title'];
-    List<gridViewTile> gridViewTiles = gridViewArguments['gridViewData'];
-
     return Scaffold(
         appBar: CustomAppBar(
-          Icon(FontAwesomePro.bars),
           context,
-          Pagetitle,
+          title,
         ),
         body: buildGridView(context, gridViewTiles));
   }
 
   Widget buildGridView(BuildContext context, List<gridViewTile> gridViewTiles) {
-    buildGridViewItems(context, gridViewTiles);
-
     return Padding(
-      padding: const EdgeInsets.all(10),
-      child: GridView.count(
-        padding: EdgeInsets.zero,
-        childAspectRatio: 3 / 2,
-        crossAxisCount: 2,
-        crossAxisSpacing: 7,
-        mainAxisSpacing: 7,
-        children: List.generate(gridViewItems.length, (index) {
-          return gridViewItems[index];
-        }),
-      ),
-    );
+        padding: const EdgeInsets.all(10),
+        child: GridView.count(
+            padding: EdgeInsets.zero,
+            childAspectRatio: 3 / 2,
+            crossAxisCount: 2,
+            crossAxisSpacing: 7,
+            mainAxisSpacing: 7,
+            children: gridViewTiles
+                .map<Widget>((tile) => buildGridViewTile(context, tile))
+                .toList()));
   }
+}
 
-  buildGridViewItems(BuildContext context, List<gridViewTile> gridViewTiles) {
-    for (int i = 0; i < gridViewTiles.length; i++) {
-      gridViewItems.add(
-        InkWell(
-          onTap: () {
-            gridViewTiles[i].Navigate(context);
-          },
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                image: DecorationImage(
-                  colorFilter: ColorFilter.mode(
-                      Colors.black.withOpacity(.68), BlendMode.darken),
-                  image: AssetImage(gridViewTiles[i].imagePath),
-                  fit: BoxFit.cover,
-                )),
-            child: Stack(
-              children: [
-                Align(
-                    alignment: FractionalOffset(.5, .35),
-                    child: gridViewTiles[i].icon),
-                Align(
-                  alignment: FractionalOffset(.5, .75),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8.0, right: 8),
-                    child: Text(
-                      gridViewTiles[i].pageTitle + '\n',
-                      textAlign: TextAlign.center,
-                      maxLines: 2,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400),
-                    ),
-                  ),
-                ),
-              ],
+buildGridViewTile(BuildContext context, gridViewTile tile) {
+  return InkWell(
+    onTap: () {
+      tile.Navigate(context);
+    },
+    child: Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          image: DecorationImage(
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(.68), BlendMode.darken),
+            image: AssetImage(tile.imagePath),
+            fit: BoxFit.cover,
+          )),
+      child: Stack(
+        children: [
+          Align(alignment: FractionalOffset(.5, .35), child: tile.icon),
+          Align(
+            alignment: FractionalOffset(.5, .75),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8.0, right: 8),
+              child: Text(
+                tile.pageTitle + '\n',
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400),
+              ),
             ),
           ),
-        ),
-      );
-    }
-  }
+        ],
+      ),
+    ),
+  );
 }
