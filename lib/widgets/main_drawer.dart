437fe9c68139714163e7cf43daa661d32df5f5ec
pwebb27 <p-webb27@hotmail.com>
2022-06-09 12:58:14 -0500

    fix: Create additional divider in MainDrawer
    - Seperate 'Social Media' into section for Facebook, Instagram, and Twitter links

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,4 +1,4 @@
-import 'package:curl_manitoba/widgets/drawer_tile.dart';
+import 'package:curl_manitoba/models/drawer_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -12,7 +12,7 @@ class MainDrawer extends StatelessWidget {
         },
         child: ListTile(
             dense: true,
-            leading: Container(alignment: Alignment.centerLeft, height: drawerTile.height, width: drawerTile.width, child:drawerTile.icon),
+            leading: Container(child:drawerTile.icon),
             title: Text(
               drawerTile.title as String,
               style: TextStyle(
@@ -26,9 +26,11 @@ class MainDrawer extends StatelessWidget {
 
   buildTiles(BuildContext context) {
     List<Widget> tiles = [];
-    for (var tile in BASIC_TILES_DATA) tiles.add(buildTile(tile, context));
-    tiles.add(buildDivider());
+    for (var tile in PAGE_TILES_DATA) tiles.add(buildTile(tile, context));
+    tiles.add(buildDivider('Useful Links'));
     for (var tile in USEFUL_LINKS_DATA) tiles.add(buildTile(tile, context));
+    tiles.add(buildDivider('Social Media'));
+    for (var tile in SOCIAL_MEDIA_LINKS_DATA) tiles.add(buildTile(tile, context));
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: tiles,
@@ -47,13 +49,13 @@ class MainDrawer extends StatelessWidget {
                       image: AssetImage("assets/images/DrawerImage.PNG"),
                       )),
               Padding(padding: EdgeInsets.only(top: 3)),
-              SingleChildScrollView(child: buildTiles(context))
+              SingleChildScrollView(physics: ScrollPhysics(), child: buildTiles(context))
             ]),
           ),
         ));
   }
 
-  Widget buildDivider() {
+  Widget buildDivider(String title) {
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
       Divider(
         height: 10,
@@ -61,7 +63,7 @@ class MainDrawer extends StatelessWidget {
       ),
       Padding(
         padding: const EdgeInsets.only(left: 8, bottom: 12, top: 10),
-        child: Text("Useful Links",
+        child: Text(title,
             style: TextStyle(
               fontSize: 14,
               color: Colors.grey.shade600,
