cd2653cfd036f0ef6c738fc9c64638838b614a9e
pwebb27 <p-webb27@hotmail.com>
2022-06-09 12:59:03 -0500

    refactor: Remove API calls and parsing data from NewsFeedScreen

--- lib/screens/news_feed_screen.dart
+++ lib/screens/news_feed_screen.dart
@@ -1,11 +1,5 @@
-import 'dart:convert';
-
-import 'package:curl_manitoba/widgets/custom_app_bar.dart';
-import 'package:curl_manitoba/widgets/font_awesome_pro_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:html/dom.dart' as dom;
-import 'package:html/parser.dart' as parser;
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../models/news_story.dart';
@@ -18,67 +12,67 @@ class NewsFeedScreen extends StatefulWidget {
 
 class _NewsFeedScreenState extends State<NewsFeedScreen> {
   static const routeName = '/news';
+  late Future<http.Response> newsStoryDataFuture;
 
-  List<NewsStory> newsStories = [];
-
-  static const URL = 'https://curlmanitoba.org/wp-json/wp/v2/posts?_fields=id,title,date,author';
-
-Future<List<dynamic>> _getDataFromWeb() async {
- var response = await 
-    http.get(Uri.parse(URL));
-  
-  return json.decode(response.body);
-}
-
-  void buildContent(List<dynamic> posts) {
-    posts.forEach((post) { 
-      newsStories.add(NewsStory.fromJson(post));
-    });
-  }
+  late List<NewsStory> newsStories;
 
   @override
   void initState() {
-    _getDataFromWeb();
+    newsStoryDataFuture = NewsStory.getNewsData(30);
+    newsStoryDataFuture
+        .then((value) => newsStories = NewsStory.parseNewsData(value));
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: _getDataFromWeb(),
+        future: newsStoryDataFuture,
         builder: (context, snapshot) {
-          if (snapshot.data == null) {
+          if (snapshot.connectionState == ConnectionState.waiting)
             return CircularProgressBar();
-          } else
-            buildContent(snapshot.data as List<dynamic>);
           return Scaffold(
               body: SingleChildScrollView(
-                physics: ScrollPhysics(),
-                child: Column(children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8,left:8,right:8),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [Text('Sign up for our ', style: TextStyle(fontSize: 16)), GestureDetector(child: Text('newsletter', style: TextStyle(fontSize: 15, color: Colors.blue)),
-                            )]),
-                            ),
-                          ListView.builder(
-                            physics: NeverScrollableScrollPhysics(),
-                            shrinkWrap: true,
+            physics: ScrollPhysics(),
+            child: Column(children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8, left: 8, right: 8),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text('Sign up for our ', style: TextStyle(fontSize: 16)),
+                  GestureDetector(
+                    child: Text('newsletter',
+                        style: TextStyle(fontSize: 15, color: Colors.blue)),
+                  )
+                ]),
+              ),
+              ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
                   itemBuilder: (ctx, index) {
-                    return buildNewsStoryItem(context,
+                    return buildNewsStoryItem(
+                      context,
                       newsStories[index],
                     );
                   },
                   itemCount: newsStories.length),
-                  Divider(),
-                  SvgPicture.asset('assets/icons/folder.svg'),
-                  Text('Looking for more news?', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold )),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 13, top: 5, left: 8, right: 8.0),
-                    child: Text('Visit the new archive for a history of published articles',textAlign: TextAlign.center, style: TextStyle(fontSize: 15),),
-                  )
-                        ]),
-              ));
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: SvgPicture.asset('assets/icons/folder.svg', height: 30),
+              ),
+              Text('Looking for more news?',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              Padding(
+                padding: const EdgeInsets.only(
+                    bottom: 13, top: 5, left: 8, right: 8.0),
+                child: Text(
+                  'Visit the new archive for a history of published articles',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 15),
+                ),
+              )
+            ]),
+          ));
         });
   }
 }
@@ -100,7 +94,7 @@ buildNewsStoryItem(BuildContext context, NewsStory newsStory) {
             Expanded(
               flex: 20,
               child: Image.network(
-                newsStory.imageURL as String,
+                newsStory.imageURL,
                 fit: BoxFit.cover,
               ),
             ),
@@ -116,15 +110,15 @@ buildNewsStoryItem(BuildContext context, NewsStory newsStory) {
                       padding: const EdgeInsets.only(bottom: 7.0),
                       child: Text(
                         newsStory.date,
-                        style:
-                            TextStyle(fontSize: 13, color: Colors.grey.shade700),
+                        style: TextStyle(
+                            fontSize: 13, color: Colors.grey.shade700),
                       ),
                     ),
                     Text(newsStory.headline + '\n\n\n',
                         maxLines: 4,
                         overflow: TextOverflow.ellipsis,
-                        style:
-                            TextStyle(fontSize: 15, fontWeight: FontWeight.bold))
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold))
                   ],
                 ),
               ),
