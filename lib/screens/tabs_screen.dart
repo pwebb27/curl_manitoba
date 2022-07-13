a6b28fe8a9749adbe8b2a2f789a087aa59295ba8
pwebb27 <p-webb27@hotmail.com>
2022-07-13 11:48:00 -0500

    perf: Implement NewsStory API call into TabsScreen

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,11 +1,11 @@
+import 'package:curl_manitoba/models/news_story.dart';
 import 'package:curl_manitoba/models/scores_competition.dart';
-import 'package:curl_manitoba/screens/news_feed_screen.dart';
+import 'package:curl_manitoba/screens/news_screen.dart';
 import 'package:curl_manitoba/screens/scores_screen.dart';
 import 'package:curl_manitoba/widgets/circular_progress_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../widgets/custom_app_bar.dart';
-import '../widgets/font_awesome_pro_icons.dart';
 import '../widgets/main_drawer.dart';
 import 'calendar_screen.dart';
 import 'e_entry_screen.dart';
@@ -18,9 +18,12 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-  late Future<http.Response> competitionDataFuture;
+  late List<Future<http.Response>> tabsScreenFutures;
   late List<scoresCompetition> loadedCompetitions;
+  late List<NewsStory> loadedNews;
   late final List<Widget> _pages;
+  late String test;
+  Future<void>? resultsFuture;
 
   int _selectedPageIndex = 0;
 
@@ -31,13 +34,17 @@ class _TabsScreenState extends State<TabsScreen> {
   }
 
   void initState() {
-    competitionDataFuture = scoresCompetition.getCompetitionData();
-    competitionDataFuture.then((data) {
-      loadedCompetitions = scoresCompetition.parseCompetitionData(data);
+    tabsScreenFutures = [
+      scoresCompetition.getCompetitionData(),
+      NewsStory.getNewsData(8)
+    ];
+    resultsFuture = Future.wait(tabsScreenFutures).then((data) {
+      loadedCompetitions = scoresCompetition.parseCompetitionData(data[0]);
+      loadedNews = NewsStory.parseNewsData(data[1]);
       _pages = [
-        HomeScreen(loadedCompetitions),
+        HomeScreen(loadedCompetitions, loadedNews),
         eEntryScreen(),
-        NewsFeedScreen(),
+        NewsFeedScreen(loadedNews),
         ScoresScreen(loadedCompetitions),
         CalendarScreen(),
       ];
@@ -63,54 +70,58 @@ class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: competitionDataFuture,
+        future:resultsFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting)
             return Scaffold(body: CircularProgressBar());
 
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
+          return DefaultTabController(
+              length: 2,
+              child: Scaffold(
                 appBar: (_selectedPageIndex != 0)
                     ? CustomAppBar(
-              context,
-              (_selectedPageIndex == 1)
-                  ? 'Electronic Entry'
-                  : (_selectedPageIndex == 2)
-                      ? 'News'
-                      : (_selectedPageIndex == 3)
-                          ? 'Live Scores & Results'
-                          : (_selectedPageIndex == 4)
-                              ? 'Calendar of Events'
-                              : "",
+                        context,
+                        (_selectedPageIndex == 1)
+                            ? 'Electronic Entry'
+                            : (_selectedPageIndex == 2)
+                                ? 'News'
+                                : (_selectedPageIndex == 3)
+                                    ? 'Live Scores & Results'
+                                    : (_selectedPageIndex == 4)
+                                        ? 'Calendar of Events'
+                                        : "",
                         (_selectedPageIndex == 3) ? true : false)
                     : null,
-          drawer: MainDrawer(),
-          body: _pages[_selectedPageIndex],
-          bottomNavigationBar: SizedBox(
-            height: 60,
-            child: Container(
-              decoration: BoxDecoration(
-                  border: Border(
+                drawer: MainDrawer(),
+                body: _pages[_selectedPageIndex],
+                bottomNavigationBar: SizedBox(
+                  height: 60,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border(
                             top: BorderSide(
                                 color: Colors.grey.shade700, width: .4))),
-              child: BottomNavigationBar(
-                elevation: 10,
-                currentIndex: _selectedPageIndex,
-                type: BottomNavigationBarType.fixed,
-                unselectedItemColor: Colors.grey.shade700,
-                selectedItemColor: Theme.of(context).primaryColor,
-                onTap: _selectPage,
-                items: [
-                  _buildBottomNavigationBarItem('Home', 'home', 23.5),
-                  _buildBottomNavigationBarItem('e-Entry', 'add-group', 24),
-                  _buildBottomNavigationBarItem('News', 'newspaper', 23),
-                  _buildBottomNavigationBarItem('Scores', 'scoreboard', 23),
-                  _buildBottomNavigationBarItem('Calendar', 'calendar-days', 25),
-                ],
-              ),
-            ),
-          ),
-        ));
+                    child: BottomNavigationBar(
+                      elevation: 10,
+                      currentIndex: _selectedPageIndex,
+                      type: BottomNavigationBarType.fixed,
+                      unselectedItemColor: Colors.grey.shade700,
+                      selectedItemColor: Theme.of(context).primaryColor,
+                      onTap: _selectPage,
+                      items: [
+                        _buildBottomNavigationBarItem('Home', 'home', 23.5),
+                        _buildBottomNavigationBarItem(
+                            'e-Entry', 'add-group', 24),
+                        _buildBottomNavigationBarItem('News', 'newspaper', 23),
+                        _buildBottomNavigationBarItem(
+                            'Scores', 'scoreboard', 23),
+                        _buildBottomNavigationBarItem(
+                            'Calendar', 'calendar-days', 25),
+                      ],
+                    ),
+                  ),
+                ),
+              ));
+        });
   }
 }
