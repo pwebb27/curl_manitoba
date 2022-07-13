bbd0f109feca47312d12dc51f0b72c1ea952992c
pwebb27 <p-webb27@hotmail.com>
2022-07-13 11:45:09 -0500

    refactor: Extract date formation into scoresCompetition class

--- lib/widgets/competition_tile.dart
+++ lib/widgets/competition_tile.dart
@@ -1,6 +1,7 @@
 import 'package:curl_manitoba/models/scores_competition.dart';
 import 'package:curl_manitoba/widgets/font_awesome_pro_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
 
 class CompetitionTile extends StatelessWidget {
@@ -10,7 +11,7 @@ class CompetitionTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left:5,right:5,bottom:0),
+      padding: const EdgeInsets.only(left: 5, right: 5, bottom: 0),
       child: InkWell(
           onTap: () {
             Navigator.pushNamed(context, '/competition',
@@ -24,59 +25,59 @@ class CompetitionTile extends StatelessWidget {
             elevation: 1.5,
             child: Padding(
               padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-              child:
-                  Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                Text(competition.name + '\n',
-                    maxLines: 2,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
-                Padding(
-                  padding: const EdgeInsets.only(top: 7),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(bottom: 1.5),
-                                  child: Icon(FontAwesomePro.calendar_range,
-                                      size: 10, color: Colors.grey.shade700),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 5),
-                                  child: Text(
-                                    DateFormat('LLL d, y')
-                                            .format(competition.startDate) +
-                                        ' - ' +
-                                        DateFormat('LLL d, y')
-                                            .format(competition.endDate),
-                                    style: TextStyle(
-                                        fontSize: 12,
-                                        color: Colors.grey.shade700),
-                                  ),
-                                ),
-                              ]),
-                              Row(children: [
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(competition.name + '\n',
+                        maxLines: 2,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15)),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 7),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(children: [
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.only(bottom: 1.5),
+                                      child: Icon(FontAwesomePro.calendar_range,
+                                          size: 10,
+                                          color: Colors.grey.shade700),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 5),
+                                      child: Text(
+                                        competition.formatDateRange(),
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            color: Colors.grey.shade700),
+                                      ),
+                                    ),
+                                  ]),
+                                  Row(children: [
                                     SvgPicture.asset(
                                         'assets/icons/landmark.svg',
                                         height: 10,
                                         color: Colors.grey.shade700),
-                                Padding(
-                                    padding: const EdgeInsets.only(left: 5),
-                                    child: Text(competition.venue,
-                                        style: TextStyle(
-                                            fontSize: 12,
-                                            color: Colors.grey.shade700)))
-                              ])
-                            ]),
-                        Image.network(
-      competition.sponsorImageUrl,
-      height: 30,
-    )
-                      ]),
-                )
-              ]),
+                                    Padding(
+                                        padding: const EdgeInsets.only(left: 5),
+                                        child: Text(competition.venue,
+                                            style: TextStyle(
+                                                fontSize: 12,
+                                                color: Colors.grey.shade700)))
+                                  ])
+                                ]),
+                            Image.network(
+                              competition.sponsorImageUrl,
+                              height: 30,
+                            )
+                          ]),
+                    )
+                  ]),
             ),
           )),
     );
