80adfbc653eecb202a0516bbe0bda6b0c136317e
pwebb27 <p-webb27@hotmail.com>
2022-07-20 17:02:17 -0500

    refactor: Change teamsScreen to teamScreen

--- lib/screens/competitionTabs/teams_screen.dart
+++ lib/screens/competitionTabs/teams_screen.dart
@@ -1,5 +1,7 @@
 import 'package:curl_manitoba/models/scores_competition.dart';
-import 'package:curl_manitoba/models/teams.dart';
+import 'package:curl_manitoba/models/team.dart';
+import 'package:curl_manitoba/screens/competition_screen.dart';
+import 'package:curl_manitoba/screens/team_screen.dart';
 import 'package:curl_manitoba/widgets/circular_progress_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +21,7 @@ class _TeamsScreenState extends State<TeamsScreen>
   bool get wantKeepAlive => true;
   late scoresCompetition competition;
   late Future<http.Response> teamsFuture;
+  
   late List<Team> teams;
   void initState() {
     teams = [];
@@ -26,26 +29,35 @@ class _TeamsScreenState extends State<TeamsScreen>
     competition = widget.competition;
     print(competition.id);
     teamsFuture = Team.fetchTeamsData(competition.id);
-    teamsFuture.then((data) {
-      teams = Team.parseTeamsData(data);
-    });
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return FutureBuilder(
         future: teamsFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting)
             return CircularProgressBar();
+          teams = Team.parseTeamsData(snapshot.data as http.Response);
           return ListView.builder(
-            shrinkWrap: true,
-            itemCount: teams.length,
-            itemBuilder: (context, index) => ListTile(
-              title: Text(teams[index].name),
-            ),
-          );
+              shrinkWrap: true,
+              itemCount: teams.length,
+              itemBuilder: (context, index) => Container(
+                    child: Center(
+                      child: ListTile(
+                        onTap: () {
+                          navKey.currentState!.push(MaterialPageRoute(
+                            builder: (_) => TeamDataScreen(teams[index]),
+                          ));
+                        },
+                        title: Text(teams[index].name),
+                        trailing: Icon(Icons.arrow_right),
+                      ),
+                    ),
+                  ));
         });
   }
 }
---
+++ lib/screens/team_screen.dart
@@ -0,0 +1,155 @@
+import 'package:curl_manitoba/models/team.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class TeamDataScreen extends StatefulWidget {
+  Team team;
+
+  TeamDataScreen(this.team);
+
+  @override
+  State<TeamDataScreen> createState() => _TeamDataScreenState();
+}
+
+class _TeamDataScreenState extends State<TeamDataScreen> {
+  late Team team;
+
+  void initState() {
+    team = widget.team;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Expanded(
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 10.0),
+          child: Text(
+            team.name,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 19,
+            ),
+          ),
+        ),
+        ListView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: team.players.length,
+          itemBuilder: ((context, index) => ListTileTheme(
+              dense: true,
+              child: Theme(
+                data: ThemeData(dividerColor: Colors.transparent),
+                child: ExpansionTile(
+                  expandedAlignment: Alignment.centerLeft,
+                  leading: InkWell(
+                      onTap: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) =>
+                                ImageDialog(team.players[index].profilePicUrl));
+                      },
+                      child: CircleAvatar(
+                        backgroundImage:
+                            NetworkImage(team.players[index].profilePicUrl),
+                      )),
+                  subtitle: Text(
+                    team.players[index].position,
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  title: Text(
+                    team.players[index].name,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 5.0),
+                      child: Text(
+                          'City: ' + (team.players[index].city as String),
+                          style: TextStyle(fontSize: 16)),
+                    ),
+                    (team.players[index].delivery != null)
+                        ? Padding(
+                            padding: const EdgeInsets.only(bottom: 5.0),
+                            child: Text(
+                                'Delivery: ' +
+                                    (team.players[index].delivery as String),
+                                style: TextStyle(fontSize: 16)))
+                        : SizedBox.shrink(),
+                    (team.players[index].club != null)
+                        ? Padding(
+                            padding: const EdgeInsets.only(bottom: 5.0),
+                            child: Text(
+                              'Club: ' + (team.players[index].club as String),
+                              style: TextStyle(fontSize: 16),
+                            ))
+                        : SizedBox.shrink()
+                  ],
+                ),
+              ))),
+        ),
+        Padding(padding: EdgeInsets.all(12)),
+        Row(children: [buildDrawsTable()]),
+      ]),
+    ));
+  }
+}
+
+class ImageDialog extends StatelessWidget {
+  String imageUrl;
+  ImageDialog(this.imageUrl);
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: CircleBorder(),
+      child: Container(
+        width: 250,
+        height: 250,
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: NetworkImage(imageUrl),
+            )),
+      ),
+    );
+  }
+}
+
+buildDrawsTable() {
+  return Expanded(
+    child: Padding(padding: EdgeInsets.symmetric(horizontal: 10),child:SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: DataTable(
+        border: TableBorder.symmetric(outside: BorderSide(width: .2)),
+        headingRowHeight: 40,
+        dataRowHeight: 40,
+        headingRowColor: MaterialStateProperty.all(Colors.grey.shade400),
+        columnSpacing: 30,
+        decoration: BoxDecoration(
+          border: Border(
+            right: BorderSide(
+              color: Colors.black,
+              width: 0.5,
+            ),
+          ),
+        ),
+        columns: [
+          DataColumn(
+              label: Text('Draw', style: TextStyle(color: Colors.white))),
+          DataColumn(
+              label: Text('Started at', style: TextStyle(color: Colors.white))),
+          DataColumn(
+              label: Text('Result', style: TextStyle(color: Colors.white))),
+          DataColumn(
+              label: Text('Score', style: TextStyle(color: Colors.white))),
+          DataColumn(
+              label: Text('Opponent', style: TextStyle(color: Colors.white))),
+        ],
+        rows: [],
+      ),
+    ),
+  ));
+}
