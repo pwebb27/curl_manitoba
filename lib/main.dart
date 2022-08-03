d6238c249beaa134e024a5a43c5183340b208e3b
pwebb27 <p-webb27@hotmail.com>
2022-08-03 10:10:41 -0500

    feat: Firebase initial setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,29 @@
 import 'package:curl_manitoba/route_generator.dart';
 import 'package:curl_manitoba/screens/mainTabs/news/news_article_screen.dart';
 import 'package:curl_manitoba/screens/tabsScreens/home_tabs_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:wakelock/wakelock.dart';
 
-void main() => runApp(MyApp());
-final GlobalKey<NavigatorState> _navKey = GlobalKey<NavigatorState>();
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
+ runApp(MyApp());
+}
+final GlobalKey<NavigatorState> _navKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Wakelock.enable();
-    
+
     return MaterialApp(
         theme: ThemeData(
             primaryColor: Color.fromRGBO(111, 18, 0, 1),
+
             colorScheme:
-                ColorScheme.fromSwatch().copyWith(secondary: Colors.white),
+                ColorScheme.fromSwatch().copyWith(secondary: Color.fromRGBO(251, 208, 7, 1)),
             fontFamily: 'Roboto'),
         initialRoute: '/',
         onGenerateRoute: RouteGenerator.generateRoute);
