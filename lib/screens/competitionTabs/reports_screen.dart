35579b73053f741212d55b2b7de474ece9a18f34
pwebb27 <p-webb27@hotmail.com>
2022-08-03 10:04:02 -0500

    feat: Create main display for ReportsScreen

--- lib/screens/competitionTabs/reports_screen.dart
+++ lib/screens/competitionTabs/reports_screen.dart
@@ -1,5 +1,6 @@
 import 'package:curl_manitoba/models/scoresCompetitionModels/scores_competition.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -7,22 +8,36 @@ class ReportsScreen extends StatefulWidget {
   scoresCompetition competition;
   ReportsScreen(this.competition);
 
-
   @override
   State<ReportsScreen> createState() => _ReportsScreenState();
 }
 
-class _ReportsScreenState extends State<ReportsScreen> with AutomaticKeepAliveClientMixin {
+class _ReportsScreenState extends State<ReportsScreen>
+    with AutomaticKeepAliveClientMixin {
   bool get wantKeepAlive => true;
   late scoresCompetition competition;
+  static const List<String> reports = [
+    'Attendance',
+    'Competition Matrix',
+    'Draw Results',
+    'Scoring Analysis',
+    'Scoring Analysis by Hammer',
+    'Team Rosters',
+    'Team Standings'
+  ];
 
-  void initState(){
+  void initState() {
     competition = widget.competition;
   }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return Center(child: Text('Reports'),);
-    
+    return ListView(children: [
+      for (String report in reports)
+        ListTile(
+          title: Text(report),
+        )
+    ]);
   }
 }
