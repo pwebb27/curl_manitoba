d47e2a2f6b99c13d12f586dcba68b469e071eb48
pwebb27 <p-webb27@hotmail.com>
2022-08-03 10:08:09 -0500

    perf: Use preloaded events for Calendar Screen

--- lib/screens/mainTabs/calendar_screen.dart
+++ lib/screens/mainTabs/calendar_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:curl_manitoba/models/calendar_event.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -8,127 +7,127 @@ import 'package:table_calendar/table_calendar.dart';
 import 'package:http/http.dart' as http;
 
 class CalendarScreen extends StatefulWidget {
+  Map<DateTime, List<CalendarEvent>> preloadedEvents;
+  CalendarScreen(this.preloadedEvents);
+
   @override
   State<CalendarScreen> createState() => _CalendarScreenState();
 }
 
-class _CalendarScreenState extends State<CalendarScreen> with AutomaticKeepAliveClientMixin {
-    bool get wantKeepAlive => true;
+class _CalendarScreenState extends State<CalendarScreen>
+    with AutomaticKeepAliveClientMixin {
+  bool get wantKeepAlive => true;
 
   late Future<http.Response> calendarDataFuture;
-  late Map<DateTime, List<CalendarEvent>> calendarEvents;
+  late Map<DateTime, List<CalendarEvent>> preLoadedEvents;
 
   late DateTime selectedDay;
   late DateTime focusedDay;
   late List<CalendarEvent> selectedEvents;
+  late Map<DateTime, List<CalendarEvent>> additionalEvents;
+  bool loadedAdditionalEvents = false;
 
   @override
   void initState() {
     super.initState();
+    preLoadedEvents = widget.preloadedEvents;
     selectedEvents = [];
     selectedDay = DateUtils.dateOnly(DateTime.now());
     focusedDay = DateUtils.dateOnly(DateTime.now());
     calendarDataFuture = CalendarEvent.getCalendarData();
     calendarDataFuture.then(
-        (value) => calendarEvents = CalendarEvent.parseCalendarData(value));
+        (value) => preLoadedEvents = CalendarEvent.parseCalendarData(value));
   }
 
   List<CalendarEvent> _getEventsFromDay(DateTime date) {
-    return calendarEvents[DateUtils.dateOnly(date)] ?? [];
+    return preLoadedEvents[DateUtils.dateOnly(date)] ?? [];
   }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return FutureBuilder(
-        future: calendarDataFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting)
-            return Center(child: CircularProgressBar());
-          return SingleChildScrollView(
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              SizedBox(
-                height: 370,
-                child: TableCalendar(
-                  shouldFillViewport: true,
-                  calendarBuilders: CalendarBuilders(
-                      singleMarkerBuilder: ((context, day, event) => Container(
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              shape: BoxShape.rectangle,
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(1.0),
-                              child: Text(
-                                  calendarEvents[DateUtils.dateOnly(day)]!
-                                      .length
-                                      .toString(),
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 12)),
-                            ),
-                          ))),
-                  focusedDay: selectedDay,
-                  firstDay: DateTime(2021, 1, 1),
-                  lastDay: DateTime(2022, 12, 31),
-                  headerStyle: HeaderStyle(formatButtonVisible: false),
-                  selectedDayPredicate: (DateTime date) {
-                    return isSameDay(selectedDay, date);
-                  },
-                  eventLoader: (day) {
-                    return _getEventsFromDay(day);
-                  },
-                  calendarStyle: CalendarStyle(
-                      markersMaxCount: 1,
-                      todayDecoration: BoxDecoration(
-                          color: Color.fromRGBO(169, 113, 102, 1),
-                          shape: BoxShape.circle),
-                      selectedDecoration: BoxDecoration(
-                          color: Theme.of(context).primaryColor,
-                          shape: BoxShape.circle)),
-                  onDaySelected: (DateTime selectDay, DateTime focusDay) {
-                    setState(() {
-                      selectedDay = selectDay;
-                      focusedDay = focusDay;
-                      selectedEvents =
-                          _getEventsFromDay(DateUtils.dateOnly(selectedDay));
-                    });
-                  },
-                ),
-              ),
-              Divider(thickness: 1, height: 40),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: Text(
-                    'Events for ' + DateFormat('LLL d, y').format(selectedDay),
+    return SingleChildScrollView(
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        SizedBox(
+          height: 370,
+          child: TableCalendar(
+            shouldFillViewport: true,
+            calendarBuilders: CalendarBuilders(
+                singleMarkerBuilder: ((context, day, event) => Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        shape: BoxShape.rectangle,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(1.0),
+                        child: Text(
+                            preLoadedEvents[DateUtils.dateOnly(day)]!
+                                .length
+                                .toString(),
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 12)),
+                      ),
+                    ))),
+            focusedDay: selectedDay,
+            firstDay: DateTime(2021, 1, 1),
+            lastDay: DateTime(2022, 12, 31),
+            headerStyle: HeaderStyle(formatButtonVisible: false),
+            selectedDayPredicate: (DateTime date) {
+              return isSameDay(selectedDay, date);
+            },
+            eventLoader: (day) {
+              return _getEventsFromDay(day);
+            },
+            calendarStyle: CalendarStyle(
+                markersMaxCount: 1,
+                todayDecoration: BoxDecoration(
+                    color: Color.fromRGBO(169, 113, 102, 1),
+                    shape: BoxShape.circle),
+                selectedDecoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    shape: BoxShape.circle)),
+            onDaySelected: (DateTime selectDay, DateTime focusDay) {
+              setState(() {
+                selectedDay = selectDay;
+                focusedDay = focusDay;
+                selectedEvents =
+                    _getEventsFromDay(DateUtils.dateOnly(selectedDay));
+              });
+            },
+   
+          ),
+        ),
+        Divider(thickness: 1, height: 40),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Text(
+              'Events for ' + DateFormat('LLL d, y').format(selectedDay),
+              style: TextStyle(fontSize: 19, fontWeight: FontWeight.w700)),
+        ),
+        ...selectedEvents.map((event) => ExpansionTile(
+                title: Text(event.eventName,
                     style:
-                        TextStyle(fontSize: 19, fontWeight: FontWeight.w700)),
-              ),
-              ...selectedEvents.map((event) => ExpansionTile(
-                      title: Text(event.eventName,
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w500)),
-                      expandedAlignment: Alignment.centerLeft,
-                      subtitle: Text(
-                          DateFormat('LLL d, y').format(event.startDate) +
-                              ' - ' +
-                              DateFormat('LLL d, y').format(event.endDate),
-                          style: TextStyle(
-                              fontSize: 13.5, color: Colors.grey.shade700)),
-                      children: [
-                        Column(
-                          children: [
-                            (event.venue != '')
-                                ? Text('Venue: ' + event.venue)
-                                : Text(''),
-                            (event.details != '')
-                                ? Text('Details: ' + event.details)
-                                : Text('')
-                          ],
-                        )
-                      ]))
-            ]),
-          );
-        });
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                expandedAlignment: Alignment.centerLeft,
+                subtitle: Text(
+                    DateFormat('LLL d, y').format(event.startDate) +
+                        ' - ' +
+                        DateFormat('LLL d, y').format(event.endDate),
+                    style:
+                        TextStyle(fontSize: 13.5, color: Colors.grey.shade700)),
+                children: [
+                  Column(
+                    children: [
+                      (event.venue != '')
+                          ? Text('Venue: ' + event.venue)
+                          : Text(''),
+                      (event.details != '')
+                          ? Text('Details: ' + event.details)
+                          : Text('')
+                    ],
+                  )
+                ]))
+      ]),
+    );
   }
 }
