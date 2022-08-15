3b643f035f02f1a0e44b27796879b63b81e9d339
pwebb27 <p-webb27@hotmail.com>
2022-08-15 12:46:46 -0500

    refactor: Use seperate appbar widget for HomeScreen

--- lib/screens/mainTabs/home_screen.dart
+++ lib/screens/mainTabs/home_screen.dart
@@ -20,20 +20,20 @@ class HomeScreen extends StatefulWidget with PreferredSizeWidget {
   Size get preferredSize => Size.fromHeight(60);
 }
 
-class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin,AutomaticKeepAliveClientMixin {
-    bool get wantKeepAlive => true;
+class _HomeScreenState extends State<HomeScreen>
+    with TickerProviderStateMixin, AutomaticKeepAliveClientMixin {
+  bool get wantKeepAlive => true;
 
   late final TabController _tabController;
-late List<scoresCompetition> loadedCompetitions;
-late List<NewsStory> loadedNews;
-
+  late List<scoresCompetition> loadedCompetitions;
+  late List<NewsStory> loadedNews;
 
   void initState() {
     loadedCompetitions = widget.loadedCompetitions;
     loadedNews = widget.loadedNews;
-    
+
     super.initState();
-  
+
     _tabController = TabController(length: 2, vsync: this);
   }
 
@@ -41,12 +41,62 @@ late List<NewsStory> loadedNews;
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-        appBar: CustomAppBar(context,''),
-        body: TabBarView(
-          children: [HomeFeedScreen(loadedCompetitions, loadedNews), TwitterFeedScreen()],
-        ),
-      
-     
+      appBar: homeTabbar(
+        _tabController,
+      ),
+      body: TabBarView(
+        children: [
+          HomeFeedScreen(loadedCompetitions, loadedNews),
+          TwitterFeedScreen()
+        ],
+      ),
+    );
+  }
+}
+
+class homeTabbar extends StatelessWidget with PreferredSizeWidget {
+  homeTabbar(this._tabController);
+  TabController _tabController;
+  @override
+  Size get preferredSize => Size.fromHeight(90);
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return Container(
+        child: AppBar(
+            leading: Builder(
+                builder: (context) => IconButton(
+                    icon: Icon(FontAwesomePro.bars, size: 24),
+                    onPressed: () => Scaffold.of(context).openDrawer())),
+            title: buildAppBarImage(),
+            bottom: PreferredSize(
+                preferredSize: Size.fromHeight(15),
+                child: Container(
+                    height: 39,
+                    color: Colors.white,
+                    child: TabBar(
+                      labelColor: Theme.of(context).colorScheme.primary,
+                        controller: _tabController,
+                        indicatorWeight: 3.5,
+                        unselectedLabelColor: Colors.grey.shade700,
+                        unselectedLabelStyle:
+                            TextStyle(fontWeight: FontWeight.w600),
+                        labelStyle: TextStyle(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                        indicatorColor: Color.fromRGBO(111, 17, 0, 1),
+                        tabs: <Widget>[
+                          Tab(child: Text('Home')),
+                          Tab(child: Text('Social Media')),
+                        ])))));
+  }
+
+  buildAppBarImage() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 1.5),
+      child: Image.asset('assets/images/Curl_Manitoba_Logo.png',
+          height: 24, fit: BoxFit.cover),
     );
   }
 }
