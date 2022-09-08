f57d29b5660105a6d6277c318d5e0dd368c51cd8
pwebb27 <p-webb27@hotmail.com>
2022-09-08 10:20:53 -0500

    chore: Allow for both post and page WordPressAPI calls

--- lib/models/apis/wordpress_api.dart
+++ lib/models/apis/wordpress_api.dart
@@ -4,13 +4,22 @@ import 'package:http/http.dart' as http;
 
 class WordPressAPI {
 
-  static String baseUrl = 'https://curlmanitoba.org/wp-json/wp/v2/pages/';
-  Future<http.Response> call(String pageNumber) async {
+  static String baseUrl = 'https://curlmanitoba.org/wp-json/wp/v2/';
+  Future<http.Response> fetchPage(String pageNumber) async {
     String URL =
-        baseUrl + pageNumber + '?_fields=content';
+        baseUrl +  'pages/' + pageNumber + '?_fields=content';
 
     final response = await http.get(Uri.parse(URL));
     return response;  
 
   }
+   Future<http.Response> fetchPost(String postId) async {
+    String URL =
+        baseUrl + 'posts/' + postId + '?_fields=content';
+
+    final response = await http.get(Uri.parse(URL));
+    return response;  
+
+  }
+
 }
