b935ee41f0c8644166c8554320aea5d651fa9b9a
pwebb27 <p-webb27@hotmail.com>
2022-09-08 09:33:36 -0500

    feature: Create basic API call needed for GridViewContentScreen

--- lib/screens/grid_view_content.dart
+++ lib/screens/grid_view_content.dart
@@ -1,6 +1,13 @@
+import 'dart:convert';
+
+import 'package:curl_manitoba/models/apis/wordpress_api.dart';
+import 'package:curl_manitoba/models/e_entry_competition.dart';
+import 'package:curl_manitoba/screens/mainTabs/e_entry_screen.dart';
 import 'package:curl_manitoba/widgets/custom_app_bar.dart';
 import 'package:curl_manitoba/widgets/font_awesome_pro_icons.dart';
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
 
 class GridViewContentScreen extends StatefulWidget {
   String pageTitle;
@@ -12,11 +19,17 @@ class GridViewContentScreen extends StatefulWidget {
 }
 
 class _GridViewContentScreenState extends State<GridViewContentScreen> {
-  
+  late WordPressAPI api;
+  late Future gridViewFuture;
+
   @override
   void initState() {
     super.initState();
     pageTitle = widget.pageTitle;
+    api = WordPressAPI();
+    gridViewFuture = api.call('1996');
+    gridViewFuture.then(
+        (response) => eEntryCompetition.parseElectronicEntryData(response));
   }
 
   late String pageTitle;
@@ -25,7 +38,6 @@ class _GridViewContentScreenState extends State<GridViewContentScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: CustomAppBar(
-         
           context,
           pageTitle,
         ),
