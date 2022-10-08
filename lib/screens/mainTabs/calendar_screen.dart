b531f6566dff93aa4e748ed7422a1ece11c6ea33
pwebb27 <p-webb27@hotmail.com>
2022-10-08 11:11:39 -0500

    refactor: Refactor code into seperate widgets and methods for readability

--- lib/screens/mainTabs/calendar_screen.dart
+++ lib/screens/mainTabs/calendar_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
 import 'package:url_launcher/url_launcher.dart';
-import '../../widgets/circular_progress_bar.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter_html/flutter_html.dart';
 
@@ -52,7 +51,23 @@ class _CalendarScreenState extends State<CalendarScreen>
 
     return SingleChildScrollView(
       child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        SizedBox(
+        buildTableCalendar(context),
+        Divider(thickness: 1, height: 40),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Text(
+              'Events for ' + DateFormat('LLL d, y').format(selectedDay),
+              style: TextStyle(fontSize: 19, fontWeight: FontWeight.w700)),
+        ),
+        ...selectedEvents.map((event) => Theme(
+            data: ThemeData().copyWith(dividerColor: Colors.transparent),
+            child: _eventTile(event)))
+      ]),
+    );
+  }
+
+  SizedBox buildTableCalendar(BuildContext context) {
+    return SizedBox(
           height: 370,
           child: TableCalendar(
             shouldFillViewport: true,
@@ -64,17 +79,16 @@ class _CalendarScreenState extends State<CalendarScreen>
                   right: -2,
                   child: Container(
                     decoration: BoxDecoration(
-                        color: Theme.of(context).textSelectionTheme.selectionColor,
+                      color:
+                          Theme.of(context).textSelectionTheme.selectionColor,
                         shape: BoxShape.circle),
                     padding: const EdgeInsets.all(5.0),
                     child: Text(
                       preLoadedEvents[DateUtils.dateOnly(day)]!
                           .length
                           .toString(),
-          
                       ),
                     ),
-                  
                 );
             })),
             focusedDay: selectedDay,
@@ -104,24 +118,12 @@ class _CalendarScreenState extends State<CalendarScreen>
               });
             },
           ),
-        ),
-        Divider(thickness: 1, height: 40),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12),
-          child: Text(
-              'Events for ' + DateFormat('LLL d, y').format(selectedDay),
-              style: TextStyle(fontSize: 19, fontWeight: FontWeight.w700)),
-        ),
-        ...selectedEvents.map((event) => Theme(
-            data: ThemeData().copyWith(dividerColor: Colors.transparent),
-            child: eventTile(event)))
-      ]),
     );
   }
 }
 
-class eventTile extends StatelessWidget {
-  const eventTile(this.event);
+class _eventTile extends StatelessWidget {
+  const _eventTile(this.event);
   final CalendarEvent event;
 
   @override
@@ -130,77 +132,8 @@ class eventTile extends StatelessWidget {
       padding: const EdgeInsets.only(bottom: 17.0),
       child: (event.htmlDescription != '')
           ? ExpansionTile(
-              title: Padding(
-                padding: const EdgeInsets.only(bottom: 3.0),
-                child: Text(event.name,
-                    style:
-                        TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
-              ),
-              subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                        DateFormat('hh:mm:ss').format(event.startDate) !=
-                                '00:00:00'
-                            ? DateFormat('LLLL d @ hh:mm aaa')
-                                    .format(event.startDate) +
-                                ' - ' +
-                                DateFormat('LLLL d @ hh:mm aaa')
-                                    .format(event.endDate)
-                            : DateFormat('LLLL d').format(event.startDate) +
-                                ' - ' +
-                                DateFormat('LLLL d').format(event.endDate),
-                        style: TextStyle(
-                            fontSize: 13.5, color: Colors.grey.shade700)),
-                    Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 6.0),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              if (event.venue != null || event.cost != null)
-                                DefaultTextStyle(
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .primary),
-                                  child: Row(
-                                    children: [
-                                      if (event.venue != null)
-                                        Row(
-                                          children: [
-                                            SvgPicture.asset(
-                                              'assets/icons/landmark.svg',
-                                              height: 13,
-                                            ),
-                                            Padding(
-                                              padding: const EdgeInsets.only(
-                                                  left: 4.0, right: 12),
-                                              child: Text(
-                                                event.venue!,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      if (event.cost != null)
-                                        Row(
-                                          children: [
-                                            SvgPicture.asset(
-                                                'assets/icons/circle-dollar.svg',
-                                                height: 14),
-                                            Padding(
-                                              padding: const EdgeInsets.only(
-                                                  left: 4.0),
-                                              child: Text(event.cost!),
-                                            ),
-                                          ],
-                                        ),
-                                    ],
-                                  ),
-                                ),
-                            ]))
-                  ]),
+                title: _TileTitle(event: event),
+                subtitle: _TileSubtitle(event: event),
               children: [
                 Column(
                   children: [
@@ -208,10 +141,9 @@ class eventTile extends StatelessWidget {
                       padding: const EdgeInsets.symmetric(horizontal: 10.0),
                       child: Html(
                           data: event.htmlDescription,
-                    
                           style: {
                             "p": Style(
-                              margin: EdgeInsets.only(bottom:8),
+                                margin: EdgeInsets.only(bottom: 8),
                             )
                           },
                           onLinkTap:
@@ -227,40 +159,42 @@ class eventTile extends StatelessWidget {
               ],
             )
           : ListTile(
-              title: Padding(
-                padding: const EdgeInsets.only(bottom: 3.0),
-                child: Text(event.name,
-                    style:
-                        TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                title: _TileTitle(
+                  event: event,
               ),
-              subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
+                subtitle: _TileSubtitle(event: event)));
+  }
+}
+
+class _TileSubtitle extends StatelessWidget {
+  const _TileSubtitle({
+    required this.event,
+  });
+
+  final CalendarEvent event;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
                     Text(
-                        DateFormat.Hms().format(event.startDate) != '00:00:00'
-                            ? DateFormat('LLLL d @ hh:mm aaa')
-                                    .format(event.startDate) +
+          DateFormat('hh:mm:ss').format(event.startDate) != '00:00:00'
+              ? DateFormat('LLLL d @ hh:mm aaa').format(event.startDate) +
                                 ' - ' +
-                                DateFormat('LLLL d @ hh:mm aaa')
-                                    .format(event.endDate)
+                  DateFormat('LLLL d @ hh:mm aaa').format(event.endDate)
                             : DateFormat('LLLL d').format(event.startDate) +
                                 ' - ' +
                                 DateFormat('LLLL d').format(event.endDate),
-                        style: TextStyle(
-                            fontSize: 13.5, color: Colors.grey.shade700)),
+          style: TextStyle(fontSize: 13.5, color: Colors.grey.shade700)),
                     Padding(
                         padding: const EdgeInsets.symmetric(vertical: 6.0),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
                               if (event.venue != null || event.cost != null)
                                 DefaultTextStyle(
                                   style: TextStyle(
                                       fontSize: 14,
                                       fontWeight: FontWeight.bold,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .primary),
+                    color: Theme.of(context).colorScheme.primary),
                                   child: Row(
                                     children: [
                                       if (event.venue != null)
@@ -271,8 +205,8 @@ class eventTile extends StatelessWidget {
                                               height: 13,
                                             ),
                                             Padding(
-                                              padding: const EdgeInsets.only(
-                                                  left: 4.0, right: 12),
+                            padding:
+                                const EdgeInsets.only(left: 4.0, right: 12),
                                               child: Text(
                                                 event.venue!,
                                               ),
@@ -282,12 +216,10 @@ class eventTile extends StatelessWidget {
                                       if (event.cost != null)
                                         Row(
                                           children: [
-                                            SvgPicture.asset(
-                                                'assets/icons/circle-dollar.svg',
+                          SvgPicture.asset('assets/icons/circle-dollar.svg',
                                                 height: 14),
                                             Padding(
-                                              padding: const EdgeInsets.only(
-                                                  left: 4.0),
+                            padding: const EdgeInsets.only(left: 4.0),
                                               child: Text(event.cost!),
                                             ),
                                           ],
@@ -296,10 +228,23 @@ class eventTile extends StatelessWidget {
                                   ),
                                 ),
                             ]))
-                  ]),
-            ),
-    );
+    ]);
   }
 }
 
+class _TileTitle extends StatelessWidget {
+  const _TileTitle({
+    required this.event,
+  }) ;
 
+  final CalendarEvent event;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 3.0),
+      child: Text(event.name,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+    );
+  }
+}
