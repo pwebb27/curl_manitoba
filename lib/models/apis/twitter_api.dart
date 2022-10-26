c890ba49cd8d7a2eaa17e4b202c47d53c8bf5ad2
pwebb27 <p-webb27@hotmail.com>
2022-10-26 18:09:32 -0500

    refactor: Clean TwitterApi class - Remove unnecessary import - Include query parameters and path as instance variable and not method parameter - Use static and const variables

--- lib/models/apis/twitter_api.dart
+++ lib/models/apis/twitter_api.dart
@@ -1,19 +1,20 @@
-import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
-class TwitterAPI{
-  final String bearerToken = 'AAAAAAAAAAAAAAAAAAAAANDnWgEAAAAAQ0%2BkRHKSb1CbJ0I1nJPhAlyBggU%3DqvnkZaPQjBQZ4DPAPDyArtDcTq2JTPkdYJoUqxTQLV921z3WuC';
-  String? responsed;
-  Future<String> callTwitterAPI(String path, Map<String,dynamic> queryParameters) async {
-        var response = await http.get(
-        Uri.https("api.twitter.com", path, queryParameters),
-        headers: {
-          "Authorization": 'Bearer $bearerToken',
-          "Content-Type": "application/json"
-        }
-        );
-        return response.body;
+class TwitterApi {
+  static const String _bearerToken =
+      'AAAAAAAAAAAAAAAAAAAAANDnWgEAAAAAQ0%2BkRHKSb1CbJ0I1nJPhAlyBggU%3DqvnkZaPQjBQZ4DPAPDyArtDcTq2JTPkdYJoUqxTQLV921z3WuC';
+  static const Map<String, String> _queryParameters = {
+    "user_id": "92376817",
+    "count": "15",
+    "tweet_mode": "extended"
+  };
+  static const _path = "1.1/statuses/user_timeline.json";
+
+  Future<http.Response> fetchTweets() async =>
+     await http
+        .get(Uri.https("api.twitter.com", _path, _queryParameters), headers: {
+      "Authorization": 'Bearer $_bearerToken',
+      "Content-Type": "application/json"
+    });
     
   }
-}
