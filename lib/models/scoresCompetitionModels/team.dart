5077941589fbb0642dca16376389cc56b4871f07
pwebb27 <p-webb27@hotmail.com>
2022-10-31 15:19:19 -0500

    refactor: Refactor Team class - Use initializer for constructor - Simplify parsing method and players/name field

--- lib/models/scoresCompetitionModels/team.dart
+++ lib/models/scoresCompetitionModels/team.dart
@@ -1,3 +1,4 @@
+
 import 'package:curl_manitoba/models/scoresCompetitionModels/game_results.dart';
 import 'package:curl_manitoba/models/scoresCompetitionModels/player.dart';
 import 'package:curl_manitoba/models/scoresCompetitionModels/scores_competition.dart';
@@ -7,41 +8,33 @@ import 'dart:convert';
 import 'package:intl/intl.dart';
 
 class Team {
-  late String? name;
-  late String id;
+  String? name;
+  final String id;
   List<Player>? players;
 
-  
   String? coach;
   String? affiliation;
   String? location;
-  
 
   Team(this.id);
 
-  Team.fromJson(Map<String, dynamic> json) {
-    id = json['id'].toString();
-    coach = json['coach'];
-    location = json['location'];
-    affiliation = json['affiliation'];
-    String tempName = json['name'];
-    List<String> wordList = tempName.split(" ");
-    (wordList[0].toLowerCase() == 'team')
-        ? name = tempName
-        : name = 'Team ' + tempName;
-    players = [];
-    for (Map<String, dynamic> athlete in json['team_athletes'])
-      players!.add(Player.fromJson(athlete));
-  }
-
-
+  Team.fromJson(Map<String, dynamic> jsonMap)
+      : id = jsonMap['id'].toString(),
+        coach = jsonMap['coach'],
+        location = jsonMap['location'],
+        affiliation = jsonMap['affiliation'],
+        name = jsonMap['name'].split(" ")[0] == 'Team'
+            ? jsonMap['name']
+            : 'Team ' + jsonMap['name'],
+        players = [
+          for (Map<String, dynamic> jsonPlayer in jsonMap['team_athletes'])
+            (Player.fromJson(jsonPlayer))
+        ];
 
   static List<Team> parseTeamsData(http.Response teamsResponse) {
-    List<dynamic> jsonList = json.decode(teamsResponse.body);
-    List<Team> teams = [];
-    for (Map<String, dynamic> team in jsonList) {
-      teams.add(Team.fromJson(team));
-    }
-    return teams;
+    return [
+      for (Map<String, dynamic> jsonTeam in json.decode(teamsResponse.body))
+        Team.fromJson(jsonTeam)
+    ];
   }
 }
