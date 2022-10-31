6c69dcace324c0c2f31b0d1aef5fdd146b8588fc
pwebb27 <p-webb27@hotmail.com>
2022-10-31 15:15:08 -0500

    refactor: Refactor endScores field and parseGameResults method

--- lib/models/scoresCompetitionModels/game_results.dart
+++ lib/models/scoresCompetitionModels/game_results.dart
@@ -13,19 +13,15 @@ class GameResults {
 
   GameResults(this.teamId, this.id, this.competitionId);
 
-  GameResults.fromJson(Map<String, dynamic> jsonMap) {
-    firstHammer = jsonMap['first_hammer'];
-    total = jsonMap['total'].toString();
-    endScores = {};
+  GameResults.fromJson(Map<String, dynamic> jsonMap)
+      : firstHammer = jsonMap['first_hammer'],
+        total = jsonMap['total'].toString(),
+        endScores = {
+          for (MapEntry entry
+              in (jsonMap['end_scores'] as List<dynamic>).asMap().entries)
+            entry.key: entry.value['score']
+        };
 
-    List<dynamic> endScoreData = jsonMap['end_scores'];
-    endScoreData.asMap().forEach((index, end) {
-      endScores![index.toString()] = end['score'].toString();
-    });
-  }
-
-  static GameResults parseGameResults(http.Response response) {
-    Map<String, dynamic> jsonMap = json.decode(response.body);
-    return GameResults.fromJson(jsonMap);
-  }
+  static GameResults parseGameResults(http.Response response) =>
+      GameResults.fromJson(json.decode(response.body));
 }
