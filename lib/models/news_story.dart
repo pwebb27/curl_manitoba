4f69da9d840b80ebb705c121808b70b2ea6c579f
pwebb27 <p-webb27@hotmail.com>
2022-10-31 13:18:55 -0500

    refactor: Refactor NewsStory class - Remove API call outside of class - Implement initializer for constructor - Make methods more succinct

--- lib/models/news_story.dart
+++ lib/models/news_story.dart
@@ -1,3 +1,4 @@
+
 import 'package:http/http.dart';
 import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
@@ -8,19 +9,18 @@ class NewsStory {
   late String imageURL;
   late String author;
   late String date;
-  late int id;
+  late String id;
   String? content;
 
-  NewsStory.fromJson(Map<String, dynamic> json) {
-    this.id = json['id'];
-    this.author = getAuthorId(json['author']);
-    this.headline = json['title']['rendered'];
-    this.date = DateFormat('LLL d, y').format(DateTime.parse(json['date']));
-    this.imageURL =
-        'https://images.thestar.com/CBZVV_aqoiPFukcZjs74JNLtlF8=/1200x798/smart/filters:cb(2700061000)/https://www.thestar.com/content/dam/thestar/sports/curling/2018/02/04/manitobas-jennifer-jones-heads-to-scotties-tournament-of-hearts-final/jennifer_jones.jpg';
-  }
+  NewsStory.fromJson(Map<String, dynamic> json)
+      : this.id = json['id'],
+        this.author = getAuthorId(json['author']),
+        this.headline = json['title']['rendered'],
+        this.date = DateFormat('LLL d, y').format(DateTime.parse(json['date'])),
+        this.imageURL =
+            'https://images.thestar.com/CBZVV_aqoiPFukcZjs74JNLtlF8=/1200x798/smart/filters:cb(2700061000)/https://www.thestar.com/content/dam/thestar/sports/curling/2018/02/04/manitobas-jennifer-jones-heads-to-scotties-tournament-of-hearts-final/jennifer_jones.jpg';
 
-  String getAuthorId(int authorId) {
+  static String getAuthorId(int authorId) {
     switch (authorId) {
       case 3:
         return 'Laurie Macdonell';
@@ -31,20 +31,12 @@ class NewsStory {
     }
   }
 
-  static Future<Response> getNewsData(int perPage) async {
-    final newsURL =
-        'https://curlmanitoba.org/wp-json/wp/v2/posts?_fields=id,title,date,author&per_page=${perPage.toString()}';
-    var response = await http.get(Uri.parse(newsURL));
-    return response;
-  }
-
   static List<NewsStory> parseNewsData(http.Response newsStoriesResponse) {
     final posts = json.decode(newsStoriesResponse.body);
-    List<NewsStory> newsStories = [];
-
-    posts.forEach((post) {
-      newsStories.add(NewsStory.fromJson(post));
-    });
-    return newsStories;
+    return [
+      posts.forEach((post) {
+        (NewsStory.fromJson(post));
+      })
+    ];
   }
 }
