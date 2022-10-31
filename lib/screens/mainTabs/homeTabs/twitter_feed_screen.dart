bc47147d791eb880937a6003fa261febf7c724fb
pwebb27 <p-webb27@hotmail.com>
2022-10-31 15:26:22 -0500

    refactor: Refactor TwitterFeedScreen - Remove API call from class - Simplify build method

--- lib/screens/mainTabs/homeTabs/twitter_feed_screen.dart
+++ lib/screens/mainTabs/homeTabs/twitter_feed_screen.dart
@@ -1,62 +1,44 @@
 import 'package:curl_manitoba/widgets/tweet_item.dart';
-import '../../../models/twitter_feed.dart';
 import '../../../models/tweet.dart';
 import '../../../models/apis/twitter_api.dart';
 import '../../../widgets/circular_progress_bar.dart';
 
 import 'package:flutter/material.dart';
-import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class TwitterFeedScreen extends StatefulWidget {
   @override
-  TwitterFeedScreenState createState() {
-    return TwitterFeedScreenState();
-  }
+  TwitterFeedScreenState createState() => TwitterFeedScreenState();
 }
 
 class TwitterFeedScreenState extends State<TwitterFeedScreen> {
-  List<dynamic>? data;
-  TwitterFeed feed = TwitterFeed();
-  TwitterAPI api = TwitterAPI();
-
-  Future<List<dynamic>> _getAPIData() async {
-    var response = await api.callTwitterAPI("1.1/statuses/user_timeline.json",
-        {"user_id": "92376817", "count": "15", "tweet_mode": "extended"});
-    return json.decode(response);
-  }
-
-  void buildContent(List<dynamic> map) {
-    for (var element in map) {
-      feed.addTweet(Tweet.fromJson(element));
-    }
-  }
+  late TwitterApi _twitterApi;
+  late Future _twitterApiDataFuture;
+  late List<Tweet> _tweets;
 
   @override
   void initState() {
     super.initState();
-    _getAPIData();
+    _tweets = [];
+    _twitterApi = TwitterApi();
+    _twitterApiDataFuture = _twitterApi.fetchTweets();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _getAPIData(),
+  Widget build(BuildContext context) =>
+     FutureBuilder(
+        future: _twitterApiDataFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting)
             return SliverFillRemaining(
                 child: Center(child: CircularProgressBar()));
-          buildContent(snapshot.data as List<dynamic>);
+          _tweets = Tweet.parseTweetData(snapshot.data as http.Response);
           return CustomScrollView(slivers: [
-                        
-                        SliverList(
-                          delegate:
-                              SliverChildBuilderDelegate(((context, index) {
-                            return TweetItem(feed.tweets[index]);
-                          }), childCount: feed.tweets.length),
-                        )
-                      ]);
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                  ((_, index) => TweetItem(_tweets[index])),
+                  childCount: _tweets.length),
+            )
+          ]);
         });
   }
-}
-
-
