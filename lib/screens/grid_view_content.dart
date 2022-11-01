d2a0451728368a5f3a99dd2476fccad0e6532f62
pwebb27 <p-webb27@hotmail.com>
2022-11-01 11:45:49 -0500

    refactor: Refactor GridViewContentScreen - Remove API call from class - Remove buildContent method - implement inside FutureBuilder instead

--- lib/screens/grid_view_content.dart
+++ lib/screens/grid_view_content.dart
@@ -1,14 +1,12 @@
 import 'dart:convert';
-
 import 'package:curl_manitoba/models/apis/wordpress_api.dart';
-import 'package:curl_manitoba/models/e_entry_competition.dart';
-import 'package:curl_manitoba/screens/mainTabs/e_entry_screen.dart';
+import 'package:curl_manitoba/providers/clients/wordpressClient.dart';
+import 'package:curl_manitoba/widgets/circular_progress_bar.dart';
 import 'package:curl_manitoba/widgets/custom_app_bar.dart';
-import 'package:curl_manitoba/widgets/font_awesome_pro_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:html/parser.dart';
-
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 class GridViewContentScreen extends StatefulWidget {
   String pageTitle;
@@ -21,41 +19,37 @@ class GridViewContentScreen extends StatefulWidget {
 
 class _GridViewContentScreenState extends State<GridViewContentScreen> {
   String? pageContent;
+  late final WordPressApi _wordPressApi;
+  late final String pageTitle;
+
+  WordPressApi api = WordPressApi();
 
   @override
   void initState() {
     super.initState();
     pageTitle = widget.pageTitle;
-
-    _getDataFromWeb();
+    _wordPressApi = WordPressApi()
+      ..client =
+          Provider.of<WordPressClientProvider>(context, listen: false).getClient();
   }
-
-  Future<Map<String, dynamic>> _getDataFromWeb() async {
-    WordPressAPI api = WordPressAPI();
-    http.Response response = await api.fetchPage('1996');
-
-    return json.decode(response.body);
-  }
-
-  void buildContent(Map<String, dynamic> contentMap) {
-    final document = parse(contentMap['content']['rendered']);
-    pageContent = parse(document.body!.text).documentElement!.text;
-  }
-
-  late String pageTitle;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: CustomAppBar(
-          context,
-          pageTitle,
-        ),
-        body: FutureBuilder(
-            future: _getDataFromWeb(),
-            builder: (context, snapshot) {
-              buildContent(snapshot.data as Map<String, dynamic>);
-              return Text(pageContent!);
-            }));
-  }
+  Widget build(BuildContext context) => Scaffold(
+      appBar: CustomAppBar(
+        context,
+        pageTitle,
+      ),
+      body: FutureBuilder(
+          future: _wordPressApi.fetchPage('1996'),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting)
+              return SliverFillRemaining(
+                  child: Center(child: CircularProgressBar()));
+            http.Response wordPressPageResponse =
+                snapshot.data as http.Response;
+            final document = parse((json.decode(wordPressPageResponse.body)
+                as Map<String, dynamic>)['content']['rendered']);
+            pageContent = parse(document.body!.text).documentElement!.text;
+            return Text(pageContent!);
+          }));
 }
