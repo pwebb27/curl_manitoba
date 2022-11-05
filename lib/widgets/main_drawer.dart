f7005323a54b9cbe1c179cc2347417ff3127441a
pwebb27 <p-webb27@hotmail.com>
2022-11-05 09:43:33 -0500

    refactor: Delete drawerTile class and implement into MainDrawer

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -72,5 +72,64 @@ class MainDrawer extends StatelessWidget {
             )),
       )
     ]);
+}
+
+class DrawerTile extends StatelessWidget {
+  late String title;
+  late Widget icon;
+
+  DrawerTile(this.title, String iconName,
+      {double width = 20, double height = 20}) {
+    this.icon = Container(
+        alignment: Alignment.centerLeft,
+        height: height,
+        width: width,
+        child: SvgPicture.asset(
+          'assets/icons/' + iconName + '.svg',
+          color: Colors.grey.shade700,
+        ));
+  }
+
+  Navigate(BuildContext context) {
+    Navigator.pushNamed(context, '/gridView', arguments: title);
+  }
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+      onTap: () {
+        Navigate(context);
+      },
+      child: ListTile(
+          dense: true,
+          leading: Container(child: icon),
+          title: Text(
+            title,
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.grey.shade700,
+              fontFamily: 'NeuzeitOffice',
+              fontWeight: FontWeight.bold,
+            ),
+          )));
+}
+
+class ExternalLinkDrawerTile extends DrawerTile {
+  String url;
+
+  ExternalLinkDrawerTile(String title, String iconName, this.url,
+      {double width = 20, double height = 20})
+      : super(title, iconName, width: width, height: height) {
+    this.url = url;
+  }
+
+  Navigate(BuildContext context) {
+    () async {
+      if (await canLaunch(url)) {
+        await launch(
+          url,
+          forceSafariVC: false,
+        );
+      }
+    };
   }
 }
