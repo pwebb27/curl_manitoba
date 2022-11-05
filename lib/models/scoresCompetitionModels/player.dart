05781b262b8782c02b388369baba6a83eb0a4dca
pwebb27 <p-webb27@hotmail.com>
2022-11-05 09:51:14 -0500

    refactor: Rename Player.fromJson constructor variable

--- lib/models/scoresCompetitionModels/player.dart
+++ lib/models/scoresCompetitionModels/player.dart
@@ -2,20 +2,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:string_extensions/string_extensions.dart';
 class Player {
-  late String name;
-  late String profilePicUrl;
-  late String position;
+  late String? name;
+  late String? profilePicUrl;
+  late String? position;
   String? city;
   String? club;
   String? delivery;
 
-  Player.fromJson(Map<String, dynamic> json) {
-    this.name = (json['athlete']['first_name'] as String).capitalize! + ' ' + (json['athlete']['last_name'] as String).capitalize!;
-    this.position = (json['position'] as String).capitalize!;
-    this.city = json['athlete']['city'];
-    this.club = json['athlete']['club_name'];
-    this.delivery = json['athlete']['delivery'];
+  Player.fromJson(Map<String, dynamic> jsonPlayer) {
+    this.name = (jsonPlayer['athlete']['first_name'] as String).capitalize! + ' ' + (jsonPlayer['athlete']['last_name'] as String).capitalize!;
+    this.position = (jsonPlayer['position'] as String).capitalize!;
+    this.city = jsonPlayer['athlete']['city'];
+    this.club = jsonPlayer['athlete']['club_name'];
+    this.delivery = jsonPlayer['athlete']['delivery'];
 
-    this.profilePicUrl = (json['athlete']['photo_url']);
+    this.profilePicUrl = (jsonPlayer['athlete']['photo_url']);
   }
 }
