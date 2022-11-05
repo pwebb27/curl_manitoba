adeac54767ef008a25a81998124149e0b6594485
pwebb27 <p-webb27@hotmail.com>
2022-11-05 09:40:48 -0500

    refactor: Remove tweet_item file and implement into twitter_feed_screen class

--- lib/screens/mainTabs/homeTabs/twitter_feed_screen.dart
+++ lib/screens/mainTabs/homeTabs/twitter_feed_screen.dart
@@ -1,8 +1,7 @@
-import 'package:curl_manitoba/widgets/tweet_item.dart';
 import '../../../models/tweet.dart';
-import '../../../models/apis/twitter_api.dart';
+import '../../../apis/twitter_api.dart';
 import '../../../widgets/circular_progress_bar.dart';
-
+import 'package:curl_manitoba/widgets/font_awesome_pro_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -41,4 +40,71 @@ class TwitterFeedScreenState extends State<TwitterFeedScreen> {
             )
           ]);
         });
+  }
+
+
+class TweetItem extends StatelessWidget {
+  final Tweet tweet;
+
+  TweetItem(this.tweet);
+
+  @override
+  Widget build(BuildContext context) =>
+     Theme(
+      data: ThemeData(fontFamily: 'NeuzeitOffice'),
+      child: InkWell(
+          child: Container(
+        color: Colors.grey.shade400,
+        child: Card(
+            margin: EdgeInsets.only(bottom: 1.3),
+            child: Container(
+              padding:
+                  const EdgeInsets.only(left: 7, right: 7, bottom: 16, top: 12),
+              child: Column(children: [
+                if (tweet.retweet)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(FontAwesomePro.retweet, size: 15, color: Colors.grey.shade700),
+                      Text('   Curl Manitoba retweeted',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              color: Colors.grey.shade700)),
+                    ],
+                  ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 5, bottom: 6),
+                  child: Row(
+                    children: [
+                      Padding(
+                          padding: const EdgeInsets.only(right: 7),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.network(
+                              tweet.profilePicURL,
+                              height: 38,
+                            ),
+                          )),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(tweet.userName,
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text(tweet.timePassed)
+                          
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                if (tweet.text != null) RichText(text:TextSpan(children: tweet.spans)),
+                if (tweet.mediaURL != "")
+                  Padding(
+                    padding: const EdgeInsets.only(top: 6),
+                    child: Image.network(tweet.mediaURL),
+                  )
+              ]),
+            )),
+      )),
+    );
   }
