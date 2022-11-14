13d53d75bd86710811d59a5e8011bc9bcfafdbb9
pwebb27 <p-webb27@hotmail.com>
2022-11-14 14:50:22 -0600

    refactor: Refactor Player - Use final keyword for instance variables - Implement initializer for constructor for Player - Use String interpolation instead of String cast

--- lib/models/scoresCompetitionModels/player.dart
+++ lib/models/scoresCompetitionModels/player.dart
@@ -1,21 +1,20 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:string_extensions/string_extensions.dart';
+
 class Player {
-  late String? name;
-  late String? profilePicUrl;
-  late String? position;
-  String? city;
-  String? club;
-  String? delivery;
+  final String? name;
+  final String? profilePicUrl;
+  final String? position;
+  final String? city;
+  final String? club;
+  final String? delivery;
 
-  Player.fromJson(Map<String, dynamic> jsonPlayer) {
-    this.name = (jsonPlayer['athlete']['first_name'] as String).capitalize! + ' ' + (jsonPlayer['athlete']['last_name'] as String).capitalize!;
-    this.position = (jsonPlayer['position'] as String).capitalize!;
-    this.city = jsonPlayer['athlete']['city'];
-    this.club = jsonPlayer['athlete']['club_name'];
-    this.delivery = jsonPlayer['athlete']['delivery'];
-
-    this.profilePicUrl = (jsonPlayer['athlete']['photo_url']);
-  }
+  Player.fromJson(Map<String, dynamic> jsonPlayer)
+      : this.name = '${jsonPlayer['athlete']['first_name']}'.capitalize! +
+            ' ' +
+            '${jsonPlayer['athlete']['last_name']}'.capitalize!,
+        this.position = '${jsonPlayer['position']}'.capitalize!,
+        this.city = jsonPlayer['athlete']['city'],
+        this.club = jsonPlayer['athlete']['club_name'],
+        this.delivery = jsonPlayer['athlete']['delivery'],
+        this.profilePicUrl = (jsonPlayer['athlete']['photo_url']);
 }
