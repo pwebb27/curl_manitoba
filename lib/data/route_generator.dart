59cfe73a56fe10150f388d7ff43d4ee41bfc7f42
pwebb27 <p-webb27@hotmail.com>
2022-11-15 16:24:09 -0600

    refactor: Format routeGenerator class

--- lib/data/route_generator.dart
+++ lib/data/route_generator.dart
@@ -2,7 +2,6 @@ import 'package:curl_manitoba/models/news_story.dart';
 import 'package:curl_manitoba/models/scoresCompetitionModels/scores_competition.dart';
 import 'package:curl_manitoba/screens/tabsScreens/competition_tabs_screen.dart';
 import 'package:curl_manitoba/screens/grid_view_content.dart';
-import 'package:curl_manitoba/screens/mainTabs/news/news_screen.dart';
 import 'package:curl_manitoba/screens/mainTabs/news/news_article_screen.dart';
 import 'package:curl_manitoba/screens/tabsScreens/home_tabs_screen.dart';
 import 'package:curl_manitoba/screens/grid_view_screen.dart';
@@ -16,25 +15,22 @@ class RouteGenerator {
       case '/':
         return MaterialPageRoute(builder: (_) => TabsScreen());
       case '/scoresWebPage':
-        return MaterialPageRoute(builder: (_) => NewsStoryScreen(args as NewsStory));
+        return MaterialPageRoute(
+            builder: (_) => NewsStoryScreen(args as NewsStory));
       case '/gridView':
-        return MaterialPageRoute(builder: (_) => GridViewScreen(args as String));
+        return MaterialPageRoute(
+            builder: (_) => GridViewScreen(args as String));
       case '/gridViewContent':
-        return MaterialPageRoute(builder: (_) => GridViewContentScreen(args as String));
+        return MaterialPageRoute(
+            builder: (_) => GridViewContentScreen(args as String));
       case '/competition':
-        return MaterialPageRoute(builder: (_) => CompetitionScreen(args as scoresCompetition));
-              case '/competition':
-        return MaterialPageRoute(builder: (_) => CompetitionScreen(args as scoresCompetition));
-
-
+        return MaterialPageRoute(
+            builder: (_) => CompetitionScreen(args as scoresCompetition));
       default:
         return _errorRoute();
     }
   }
 
-  static Route<dynamic> _errorRoute() =>
-     MaterialPageRoute(builder: (_) =>
-       Scaffold(body: Center(child: Text('ERROR')))
-    );
-  }
-
+  static Route<dynamic> _errorRoute() => MaterialPageRoute(
+      builder: (_) => Scaffold(body: Center(child: Text('ERROR'))));
+}
