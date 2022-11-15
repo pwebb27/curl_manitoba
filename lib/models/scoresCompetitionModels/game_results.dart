81af3b925e0029677e2a09292935840d6a8e091c
pwebb27 <p-webb27@hotmail.com>
2022-11-15 16:02:30 -0600

    refactor: Refactor GameResults class - Make main constructor fromJson (extracted from Game class) - Change previous fromJson method to augment to existing GameResults instance - Remove unnecessary parseGameResults method

--- lib/models/scoresCompetitionModels/game_results.dart
+++ lib/models/scoresCompetitionModels/game_results.dart
@@ -1,27 +1,27 @@
-import 'package:curl_manitoba/models/scoresCompetitionModels/scores_competition.dart';
-import 'package:curl_manitoba/models/scoresCompetitionModels/team.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class GameResults {
-   String? id;
-   String? teamId;
-   String? competitionId;
-   Map<String, String>? endScores;
-   bool? firstHammer;
-   String? total;
+  String? id;
+  String? teamId;
+  String? competitionId;
+  Map<int, int>? endScores;
+  bool? firstHammer;
+  String? total;
 
-  GameResults(this.teamId, this.id, this.competitionId);
+  GameResults.fromJson(
+      Map<String, dynamic> jsonGamePositions, this.competitionId)
+      : teamId = '${jsonGamePositions['competition_team_id']}',
+        id = '${jsonGamePositions['id']}';
 
-  GameResults.fromJson(Map<String, dynamic> jsonGameResults)
-      : firstHammer = jsonGameResults['first_hammer'],
-        total = jsonGameResults['total'].toString(),
-        endScores = {
-          for (MapEntry entry
-              in (jsonGameResults['end_scores'] as List<dynamic>).asMap().entries)
-            entry.key: entry.value['score']
-        };
-
-  static GameResults parseGameResults(http.Response response) =>
-      GameResults.fromJson(json.decode(response.body));
+  addVariablesFromGamesResultsResponse(http.Response response) {
+    Map<String, dynamic> jsonGameResults = json.decode(response.body);
+    firstHammer = jsonGameResults['first_hammer'];
+    total = '${jsonGameResults['total']}';
+    endScores = {
+      for (MapEntry entry
+          in (jsonGameResults['end_scores'] as List<dynamic>).asMap().entries)
+        entry.key: entry.value['score']
+    };
+  }
 }
