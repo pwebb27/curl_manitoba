279a75899c932b545ec15cbdd22d8a36966b2f8f
pwebb27 <p-webb27@hotmail.com>
2022-11-29 11:41:44 -0600

    feat: Create api base helper class - Integrate seperate class for api calls that handles exceptions

---
+++ lib/apis/api_base_helper.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+class ApiBaseHelper {
+  Client client;
+  ApiBaseHelper(this.client);
+
+  Future<http.Response> callApi(
+      {required String url,
+      Map<String, String> queryParameters = const {}}) async {
+    try {
+      final http.Response response =
+          await http.get(Uri.https(url, '', queryParameters));
+      switch (response.statusCode) {
+        case 200:
+          return response;
+        default:
+          throw Exception(
+              'Error occured while Communication with Server with StatusCode : ${response.statusCode}');
+      }
+    } on SocketException {
+      throw Failure('No Internet connection');
+     } 
+    //on FormatException {
+    //   throw Failure("Bad response format");
+    // }
+  }
+}
+
+class Failure {
+  final String message;
+  Failure(this.message);
+}
