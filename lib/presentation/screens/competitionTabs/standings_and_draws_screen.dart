2a50dfd9d95460d99e2129e7c7362188d13644be
pwebb27 <p-webb27@hotmail.com>
2023-06-14 10:21:06 -0500

    chore: Minor import changes

--- lib/presentation/screens/competitionTabs/standings_and_draws_screen.dart
+++ lib/presentation/screens/competitionTabs/standings_and_draws_screen.dart
@@ -1,28 +1,29 @@
 import 'package:curl_manitoba/domain/entities/scoresCompetitionModels/scores_competition.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class StandingsAndDrawsScreen extends StatefulWidget {
   scoresCompetition competition;
-StandingsAndDrawsScreen(this.competition);
+  StandingsAndDrawsScreen(this.competition);
   @override
-  State<StandingsAndDrawsScreen> createState() => _StandingsAndDrawsScreenState();
+  State<StandingsAndDrawsScreen> createState() =>
+      _StandingsAndDrawsScreenState();
 }
 
-class _StandingsAndDrawsScreenState extends State<StandingsAndDrawsScreen> with AutomaticKeepAliveClientMixin{
+class _StandingsAndDrawsScreenState extends State<StandingsAndDrawsScreen>
+    with AutomaticKeepAliveClientMixin {
   bool get wantKeepAlive => true;
   late scoresCompetition competition;
   @override
-
   void initState() {
     competition = widget.competition;
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return Center(child: Text('Standings'),);
-    
+    return Center(
+      child: Text('Standings'),
+    );
   }
 }
